2eeea90214a585d849c267820aa4a0435016e634
Nathaniel Truitt <nathantruitt2@gmail.com>
2023-01-24 17:13:44 -0600

    adding email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < ApplicationRecord
   belongs_to :organizations, class_name: 'Organization', foreign_key: 'organization_id'
   validates :first_name, present: true
   validates :last_name, present: true
-  validates :email, present: true
+  validates :email, present: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
   validates :organization_id, present: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, present: true
   validates :last_name, present: true
   validates :username, present: true
-  validates :email, present: true
+  validates :email, present: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address'}
   validates :organization_id, present: true
   validates :permission_id, present: true
 end
